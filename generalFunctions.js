884d7e47488ab954a666a55d3950fa175a9a1d77
gordonzev914-lang <gordonzev914@gmail.com>
2025-12-25 10:14:48 +0200

    Two functions for reading and writing to a json file

---
+++ generalFunctions.js
@@ -0,0 +1,25 @@
+import fs from "fs/promises"
+
+export const writingToJson=(Object,fileName)=>{
+    try{
+        const objectToString=JSON.stringify(Object)
+        fs.writeFile(`${fileName}.json`,objectToString ,'utf-8',(objectToString)=>{console.log(objectToString)})
+                return objectToString
+            }
+            catch(err){
+                console.log(err)
+            }
+        }
+
+export const readJsonFile=async(path)=>{
+    try{
+        const data=await fs.readFile(`${path}.json`,'utf-8',()=>{})
+        const data2=JSON.parse(data)
+        return data2
+    }
+    catch(err){
+        console.log(err)
+    }
+}//בעת קריאה לפונקציה יש להשתמש ב 
+// "await"
+
